4645c489624884d526566e90ff216d0abcc33d0d
opptutu <opptutu@gmail.com>
2015-09-24 21:01:50 -0700

    First Git commit, temo-1.

--- tests/tests.js
+++ tests/tests.js
@@ -2,3 +2,10 @@
  * Created by tim on 9/24/15.
  * Insane test
  */
+var should = require('should');
+
+(5).should.be.exactly(5).and.be.a.Number();
+
+var should = require('should/as-function');
+
+should(10).be.exactly(5).and.be.a.Number();
